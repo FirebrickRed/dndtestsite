9e1dad28dc3106cbfe423da81108dba7602f6f40
Jessica <36863810+FirebrickRed@users.noreply.github.com>
2021-11-07 07:28:14 -0700

    Create defaults.js

---
+++ defaults.js
@@ -0,0 +1,28 @@
+
+export const classes = [
+  "Artificer",
+  "Barbarian",
+  "Bard",
+  "Cleric",
+  "Druid",
+  "Fighter",
+  "Monk",
+  "Paladin",
+  "Ranger",
+  "Rogue",
+  "Sorcerer",
+  "Warlock",
+  "Wizard",
+];
+
+export const races = [
+  "Dwarf",
+  "Elf",
+  "Hafling",
+  "Human",
+  "Dragonborn",
+  "Gnome",
+  "Half-Elf",
+  "Half-Orc",
+  "Tiefling",
+];
