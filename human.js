23bd54448225245d0015f5bb4f9bddac8f83a9ea
Jessica <36863810+FirebrickRed@users.noreply.github.com>
2021-11-07 11:02:53 -0700

    Create human.js

---
+++ human.js
@@ -0,0 +1,12 @@
+
+export default const human {
+  // Ability Score Increase: Increase each of your ability scores by 1
+  const averageWeight = 175;
+  const matureAge = 17;
+  const oldAge = 100;
+  const defaultAlignment = ["neutral", "neutral"];
+  const size = "Medium";
+  const averageHeight = 5;
+  const speed = 30;
+  const languages = ["Common", "one Extra"];
+}
